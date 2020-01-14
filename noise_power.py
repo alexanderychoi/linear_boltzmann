58bc28487d32596c5e1817eea1ded16e412680dc
YOUR NAME <aychoi@caltech.edu>
2020-01-13 22:34:19 -0800

    Added central difference modification.

--- noise_power.py
+++ noise_power.py
@@ -87,20 +87,81 @@ def calc_sparsity():
         print('For row {:d}, the number of nozero elements is {:f}'.format(ik+1, nelperrow[ik]))
     return sparsity, nelperrow
 
-def centraldiff_matrix():
+
+def centraldiff_matrix(fullkpts_df,step_size):
+    # Do not  flush the memmap it will overwrite consecutively. 
     matrix = np.memmap(data_loc + 'scattering_matrix.mmap', dtype='float64', mode='r+', shape=(nkpts, nkpts))
-    # Get the first and last rows since these are different because of the IC
-    matrix[0,1] = matrix[0,1]-1/2
-    matrix[nkpts-2,nkpts-2] = matrix[nkpts-2,nkpts-2] + 1/2
-    # Subtract the tridiagonal from the original scattering matrix
-    for k in range(1,nkpts-1):
-        kind = k + 1
-        istart = time.time()
-        matrix[k,k-1] = matrix[k,k-1]+1/2
-        matrix[k,k+1] = matrix[k,k+1]-1/2
-        if kind % 100 == 0:
-            print('Finished k={:d}'.format(kind))
+    # Get the first and last rows since these are different because of the IC. Go through each.
+
+    # Get the unique ky and kz values from the array for looping.
+    kptdata = fullkpts_df[['k_inds', 'kx [1/A]', 'ky [1/A]', 'kz [1/A]']]
+    uniq_yz = np.unique(kptdata[['ky [1/A]', 'kz [1/A]']].values, axis=0)
+    start = time.time()
+
+    # If there are too few points in a slice < 5, we want to keep track of those points
+    shortslice_inds = []
+    lastslice_inds =[]
+
+    # Loop through the unique ky and kz values
+    for i in range(len(uniq_yz)):
+        kind = i + 1
+        ky, kz = uniq_yz[i, 0], uniq_yz[i, 1]
+
+        # Grab the "slice" of points in k space with the same ky and kz coordinate
+        slice_df = kptdata.loc[(kptdata['ky [1/A]'] == ky) & (kptdata['kz [1/A]'] == kz)]
+        slice_inds = slice_df['k_inds'].values
+
+        # if 0 in slice_inds or 1 in slice_inds or len(kptdata) in slice_inds or len(kptdata)-1 in slice_inds:
+        #     lastslice_inds.append(slice_inds)
+        #     continue
+
+        if len(slice_inds) > 4:
+            # Subset is the slice sorted by kx value in ascending order. The index of subset still references kptdata.
+            subset = slice_df.sort_values(by=['kx [1/A]'],ascending=True)
+            ordered_slice_inds = subset.index
+
+            # Set the "initial condition" i.e. the point with the most negative kx value is treated as being zero
+            # (and virtual point below)
+            matrix[ordered_slice_inds[0], ordered_slice_inds[1]] = matrix[ordered_slice_inds[0],ordered_slice_inds[1]] - \
+                                                                  1/(2*step_size)
+            matrix[ordered_slice_inds[1], ordered_slice_inds[2]] = matrix[ordered_slice_inds[1],ordered_slice_inds[2]] - \
+                                                                  1/(2*step_size)
+
+            # Set the other "boundary condition" i.e. the point with the most positive kx value is treated as being zero
+            # (and virtual point above)
+            last = len(ordered_slice_inds) - 1
+            slast = len(ordered_slice_inds) - 2
+            matrix[ordered_slice_inds[last], ordered_slice_inds[slast]] = \
+                matrix[ordered_slice_inds[last], ordered_slice_inds[slast]] + 1/(2*step_size)
+            matrix[ordered_slice_inds[slast], ordered_slice_inds[slast-1]] = \
+                matrix[ordered_slice_inds[slast], ordered_slice_inds[slast-1]] + 1/(2*step_size)
+
+            # Set the value of all other points in the slice
+            inter_inds = ordered_slice_inds[2:slast]
+            inter_inds_up = ordered_slice_inds[3:last]
+            inter_inds_down = ordered_slice_inds[1:slast-1]
+
+            # print('Ordered_slice_inds')
+            # print(*ordered_slice_inds)
+            # print('inter_inds')
+            # print(*inter_inds)
+            # print('inter_inds_up')
+            # print(*inter_inds_up)
+            # print('inter_inds_down')
+            # print(*inter_inds_down)
+
+            matrix[inter_inds, inter_inds_up] = matrix[inter_inds, inter_inds_up] - 1/(2*step_size)
+            matrix[inter_inds, inter_inds_down] = matrix[inter_inds, inter_inds_down] + 1/(2*step_size)
+
+        else:
+            shortslice_inds.append(slice_inds)
+
+        end = time.time()
+        if kind % 10 == 0:
+            print('Finished {:d} out of {:d} slices in {:.3f}s'.format(kind, len(uniq_yz),end-start))
     print('Scattering matrix modified to incorporate central difference contribution.')
+    print('Not applied to {:d} points because fewer than 5 points on the slice.'.format(len(shortslice_inds)))
+    return shortslice_inds
 
 
 if __name__ == '__main__':
@@ -121,8 +182,8 @@ if __name__ == '__main__':
     print('bumdiddly')
 
 
-    matrix = np.memmap(data_loc + 'scattering_matrix.mmap', dtype='float64', mode='r+', shape=(nkpts, nkpts))
+    # matrix = np.memmap(data_loc + 'scattering_matrix.mmap', dtype='float64', mode='r+', shape=(nkpts, nkpts))
 
-    centraldiff_matrix()
+    centraldiff_matrix(cartkpts,1)
 
     # steady_state_solns(kinds, nkpts, cartkpts, 1)
