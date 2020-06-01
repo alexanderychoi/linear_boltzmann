7ff176337b0b0990ac58390eb4ec5e4da1a15584
AlexanderYChoi <aychoi@caltech.edu>
2020-06-01 13:14:54 -0500

    New boolean for l valley derivative.

--- problem_parameters.py
+++ problem_parameters.py
@@ -3,8 +3,23 @@ import os
 import numpy as np
 import re
 
-f = open('problem_parameters.txt')
+# Problem setup. Fields, frequencies, valleys.
+fieldVector = np.array([1e2, 1e3, 1e4, 2e4, 3e4, 4e4, 5e4, 6e4, 7e4, 8e4, 9e4, 1e5,2e5,2.5e5,3e5,3.5e5,4e5,4.5e5])
 
+# fieldVector = np.array([1e2,1e3,1e4,1e5,2e5])
+freqGHz = 5
+getX = False
+derL = True
+# inputLoc = 'E:/Dropbox (Minnich Lab)/Alex_Peishi_Noise_Calcs/BoltzmannGreenFunctionNoise/2_Problem_PERT/0_Data/'
+# outputLoc = 'E:/Dropbox (Minnich Lab)/Alex_Peishi_Noise_Calcs/BoltzmannGreenFunctionNoise/2_Problem_PERT/1_Pipeline/Output/'
+
+# inputLoc = 'E:/Dropbox (Minnich Lab)/Alex_Peishi_Noise_Calcs/BoltzmannGreenFunctionNoise/2_Problem_PERT_Lder/0_Data/'
+# outputLoc = 'E:/Dropbox (Minnich Lab)/Alex_Peishi_Noise_Calcs/BoltzmannGreenFunctionNoise/2_Problem_PERT_Lder/1_Pipeline/Output/'
+
+inputLoc = 'E:/Dropbox (Minnich Lab)/Alex_Peishi_Noise_Calcs/BoltzmannGreenFunctionNoise/2_Problem/0_Data/'
+outputLoc = 'E:/Dropbox (Minnich Lab)/Alex_Peishi_Noise_Calcs/BoltzmannGreenFunctionNoise/2_Problem/1_Pipeline/Output/'
+
+f = open(inputLoc+'problem_parameters.txt')
 alltext = f.read()
 scratchLoc = re.findall(r"\s*scratchLoc\s*=\s*'(.+)'\n", alltext)
 T = float(re.findall(r"\s*Temperature\s*=\s*(.+)\n", alltext)[0])
@@ -12,18 +27,6 @@ mu = float(re.findall(r"\s*FermiLevel\s*=\s*(.+)\n", alltext)[0])
 b = float(re.findall(r"\s*GaussianBroadening\s*=\s*(.+)\n", alltext)[0])
 kgrid = float(re.findall(r"\s*GridDensity\s*=\s*(\d+)\n", alltext)[0])
 cutoff = float(re.findall(r"\s*EnergyWindow\s*=\s*(.+)\n", alltext)[0])
-
-if scratchLoc:
-	scratchLoc = scratchLoc[0]
-	print('Scratch location is \'' + scratchLoc + '\'')
-
-# Problem setup. Fields, frequencies, valleys.
-fieldVector = np.array([1e2, 1e3, 1e4, 2e4, 3e4, 4e4, 5e4, 6e4, 7e4, 8e4, 9e4, 1e5,2e5,2.5e5,3e5,3.5e5,4e5,4.5e5])
-freqGHz = 5
-getX = False
-inputLoc = 'E:/Dropbox (Minnich Lab)/Alex_Peishi_Noise_Calcs/BoltzmannGreenFunctionNoise/2_Problem_PERT/0_Data/'
-outputLoc = 'E:/Dropbox (Minnich Lab)/Alex_Peishi_Noise_Calcs/BoltzmannGreenFunctionNoise/2_Problem_PERT/1_Pipeline/Output/'
-
 print('\nData for this run loaded from "{:s}"\n'.format(inputLoc))
 print('Lattice temperature is {:.1f} K'.format(T))
 print('Fermi Level is {:.5f} eV'.format(mu))
@@ -37,14 +40,16 @@ if scratchLoc:
 simpleBool = True
 # Should we apply the scmVal factor to the scattering rates as a correction? False = Don't apply factor.
 scmBool = False
-scmVal = (2*np.pi)**2*2
+scmVal = (2*np.pi)**2/1.5*1.2298  # Problem 2
 # Convergence parameters for the GMRES solver
-relConvergence = 5e-3
+relConvergence = 1e-4
 absConvergence = 1e-60
 # Should we calculate and store the residual error?
-verboseError = False
+verboseError = True
 # What's the name of the matrix?
-scmName = 'scattering_matrix.mmap'
+# scmName = 'scatt_mat_pert.mmap'  # Problem 2 Pert
+scmName = 'scattering_matrix_simple_2.mmap'  # Problem 2
+
 # String for title of plots
 title_str = 'Grid={:.0f}^3, mu={:.4f} eV, {:.1f} K, Emax={:.3f} eV'.format(kgrid, mu, T, cutoff)
 
